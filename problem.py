65a509cae111d2a2431cea0a9c180e2117e31d56
zack242 <zakaria_009@hotmail.fr>
2023-02-21 20:44:20 +0100

    add problem and first estimator

--- problem.py
+++ problem.py
@@ -1,23 +1,34 @@
 import os
-import datetime
-import warnings
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold
-from sklearn.metrics import log_loss, recall_score, precision_score
 import rampwf as rw
+from data_cleaning import *
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import r2_score
 from rampwf.score_types.base import BaseScoreType
-from rampwf.score_types.classifier_base import ClassifierBaseScoreType
-from rampwf.workflows.sklearn_pipeline import SKLearnPipeline
-from rampwf.workflows.sklearn_pipeline import Estimator
-from preprocess import preprocess_df, get_labels
+
 
 problem_title = "Quality of Air"
-data_path = os.path.join("Data", "merged_data.csv")
+Predictions = rw.prediction_types.make_regression()
+workflow = rw.workflows.Regressor()
 
-# -----------------------------------------------------------------------------
-# Training / testing data reader
-# -----------------------------------------------------------------------------
+
+class R2(BaseScoreType):
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+    name = "R2"
+
+    def __init__(self, precision=4):
+        self.precision = precision
+
+    def __call__(self, y_true, y_pred):
+        r_2 = r2_score(y_true, y_pred)
+        return r_2
+
+
+score_types = [
+    R2(),
+]
 
 
 def get_cv(X, y):
@@ -26,16 +37,18 @@ def get_cv(X, y):
 
 
 def _read_data(path, type_):
+    data_path = os.path.join("Data", "merged_data.csv")
+    _target_column_name = "PM2.5"
 
     data = pd.read_csv(data_path, index_col=0, parse_dates=True)
 
     if type_ == "train":
         data_train, _ = preprocess_df(data)
-        X, y = get_labels(data_train, "PM2.5")
+        X, y = get_labels(data_train, _target_column_name)
 
     elif type_ == "test":
         _, data_test = preprocess_df(data)
-        X, y = get_labels(data_test, "PM2.5")
+        X, y = get_labels(data_test, _target_column_name)
 
     # for the "quick-test" mode, use less data
     test = os.getenv("RAMP_TEST_MODE", 0)
@@ -47,9 +60,9 @@ def _read_data(path, type_):
     return X, y
 
 
-def get_train_data(path="."):
-    return _read_data(path, "train")
+def get_train_data():
+    return _read_data("train")
 
 
-def get_test_data(path="."):
-    return _read_data(path, "test")
+def get_test_data():
+    return _read_data("test")
